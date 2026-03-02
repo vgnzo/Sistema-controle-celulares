d92671588025c060ee27494d67827eee5f4dfa31
Vinicius Galdino <vinigcgaldino@gmail.com>
2026-03-02 07:19:53 -0300

    config: Configurar URL da API para produção

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api';
-
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
 const api = axios.create({
     baseURL: API_URL, 
     headers: {
@@ -44,4 +43,3 @@ export const entregaService = {
 };
 
 export default api;
-
