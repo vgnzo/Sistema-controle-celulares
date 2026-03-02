fa6a3105cad31f46636f8026c541c369a211ab8f
Vinicius Galdino <vinigcgaldino@gmail.com>
2026-03-02 09:28:15 -0300

    fix api url definitiva

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
-const api = axios.create({
+const API_URL = 'https://sistema-controle-celulares.onrender.com/api';const api = axios.create({
     baseURL: API_URL, 
     headers: {
         'Content-Type': 'application/json'
