059e625d3b57f5d5f37339a1e96c6096ff495cc4
Vinicius Galdino <vinigcgaldino@gmail.com>
2026-03-02 10:02:49 -0300

    Atualiza CORS para aceitar Vercel deploy

--- backend/src/main/java/com/transpiratininga1/controlecelular/config/CorsConfig.java
+++ backend/src/main/java/com/transpiratininga1/controlecelular/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class CorsConfig {
 
@@ -13,11 +14,12 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins(
-    "http://localhost:3000",
-    "https://sistema-controle-celulares.vercel.app",
-    "https://sistema-controle-celula-git-3453df-vinigcgaldino-4283s-projects.vercel.app"
-)
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://sistema-controle-celulares.vercel.app",
+                            "https://sistema-controle-celula-git-3453df-vinigcgaldino-4283s-projects.vercel.app",
+                            "https://sistema-controle-celulares-wg05m65mo.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
