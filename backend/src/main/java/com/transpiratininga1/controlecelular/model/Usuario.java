090bdea14e9358537e1d2698f42129c333a89228
Vinicius Galdino <vinigcgaldino@gmail.com>
2026-03-03 20:20:03 -0300

    fix: remove AllArgsConstructor do Usuario

--- backend/src/main/java/com/transpiratininga1/controlecelular/model/Usuario.java
+++ backend/src/main/java/com/transpiratininga1/controlecelular/model/Usuario.java
@@ -3,13 +3,11 @@ package com.transpiratininga1.controlecelular.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
 @Entity
 @Table(name = "usuario")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class Usuario {
 
     @Id
@@ -19,9 +17,9 @@ public class Usuario {
     @Column(unique = true, nullable = false, length = 50)
     private String username;
 
-    @Column(nullable = false)
+    @Column(name = "senha", nullable = false)
     private String senha;
 
     @Column(nullable = false, length = 10)
-    private String tipo = "USER"; // ADMIN ou USER
+    private String tipo = "USER";
 }
