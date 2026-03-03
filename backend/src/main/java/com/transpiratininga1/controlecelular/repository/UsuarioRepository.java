076edc273eec2bb4e9b44ea3db3d32e944fe4403
Vinicius Galdino <vinigcgaldino@gmail.com>
2026-03-03 15:53:05 -0300

    fix: query explicita usuario e desabilitar httpBasic

--- backend/src/main/java/com/transpiratininga1/controlecelular/repository/UsuarioRepository.java
+++ backend/src/main/java/com/transpiratininga1/controlecelular/repository/UsuarioRepository.java
@@ -4,9 +4,13 @@ import com.transpiratininga1.controlecelular.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByUsername(String username);
+       @Query("SELECT u FROM Usuario u WHERE u.username = :username")
+    Optional<Usuario> findByUsername(@Param("username") String username);
+    
     boolean existsByUsername(String username);
 }
